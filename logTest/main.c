cbd916ba9e90e6975ceda3a9086313134dc6a143
Beta-Tao <wangtao_0902@163.com>
2018-10-10 00:09:42 +0800

    fflush文件，可以正常log

--- logTest/main.c
+++ logTest/main.c
@@ -5,15 +5,18 @@
 
 int main(void)
 {
-    FILE *fp = fopen("timelog.txt", "a");
-    struct tm *pNewTime = NULL;
+    FILE *fp = fopen("timelog.txt", "a+");
+    struct tm *pNewTime;
     time_t newTime;
+
+    //对文件指针进行判断
     if (fp == NULL)
     {
         perror("Open timelog.txt: ");
         exit(1);
     }
 
+    //死循环打印log
     while (1)
     {
         //printf("Hello\n");
@@ -22,8 +25,9 @@ int main(void)
         pNewTime = localtime(&newTime);
         //printf("year: %d\n", pNewTime->tm_year);
         fprintf(fp, "%d-%d-%d %d:%d:%d\n", 
-                pNewTime->tm_year, pNewTime->tm_mon, pNewTime->tm_mday, 
+                pNewTime->tm_year + 1900, pNewTime->tm_mon + 1, pNewTime->tm_mday, 
                 pNewTime->tm_hour, pNewTime->tm_min, pNewTime->tm_sec);
+        fflush(fp);
         sleep(1);
     }
     return 0;
